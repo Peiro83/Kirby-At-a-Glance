e105bfb0f7efe4cc497fa41cf69a07e0c3198c94
dmotion <info@dmotion.com>
2016-04-18 22:32:31 +0200

    Altered table logics.
    Altered table logics.

--- site/widgets/glance/glance.php
+++ site/widgets/glance/glance.php
@@ -19,9 +19,9 @@ return array(
 /*  $site = kirby()->site(); // $site must be global in order to place it in the link-options */
     $count_chapters = $site->pages()->count();
     $count_pages = $site->index()->count();
-    $count_documents = $site->index()->files()->count();
+    $count_files = $site->index()->files()->count();
     $count_images = $site->index()->files()->filterBy('type', 'image')->count();
-    $count_files = $site->index()->files()->filterBy('type', 'document')->count();
+    $count_documents = $site->index()->files()->filterBy('type', 'document')->count();
     $count_users = $site->users()->count();
 
 /*  $count_subpages = $site->grandchildren()->count(); // not needed, yet.. */
@@ -44,9 +44,9 @@ return array(
             "<table>".
               "<tr><td>Chapters</td><td><b>$count_chapters</b></td></tr>".
               "<tr><td class=\"indent\">Pages</td><td><b>$count_pages</b></td></tr>".
-              "<tr><td>Documents</td><td><b>$count_documents</b></td></tr>".
+              "<tr><td>Files</td><td><b>$count_files</b></td></tr>".
               "<tr><td class=\"indent\">Images</td><td><b>$count_images</b></td></tr>".
-              "<tr><td class=\"indent\">Files</td><td><b>$count_files</b></td></tr>".
+              "<tr><td class=\"indent\">Documents</td><td><b>$count_documents</b></td></tr>".
               "<tr><td>Users</td><td><b>$count_users</b></td></tr>".
             "</table>";
     }
